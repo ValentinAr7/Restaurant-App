b79a81f296f9592cb7dff1eec37731a306240c13
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-05-19 19:36:00 +0300

    changes

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='flex-1 bg-white' style={{backgroundColor: '#77685F' }}>
+      <SafeAreaView className='flex-1'>
+        <View>
+          <Text>Bon Apetit!</Text>
+        </View>
+      </SafeAreaView>
+
+
+      <View className='flex-row bg-white px-8 pt-8' 
+      style={{borderTopLeftRadius: 50, borderTopRightRadius: 50}}>
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#77685F',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   }, 
+  
+//   flexContainer: {
+//     flex: 1,
+//     width: 200,
+//     height: 200
+//   },
+
+// });
